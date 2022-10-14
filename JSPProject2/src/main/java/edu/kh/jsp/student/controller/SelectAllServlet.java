3a6e90fe7921297823914710133ce6c9e164c245
rkdalsrb65 <rkdalsrb65@naver.com>
2022-10-14 16:40:01 +0900

    SelectAllServlet

--- JSPProject2/src/main/java/edu/kh/jsp/student/controller/SelectAllServlet.java
+++ JSPProject2/src/main/java/edu/kh/jsp/student/controller/SelectAllServlet.java
@@ -1,6 +1,7 @@
 package edu.kh.jsp.student.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.kh.jsp.student.model.service.StudentService;
+import edu.kh.jsp.student.model.vo.Student;
 
 @WebServlet("/student/selectAll")
 public class SelectAllServlet extends HttpServlet{
@@ -17,9 +19,27 @@ public class SelectAllServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		// 서비스 객체 생성
-		StudentService service = new StudentService();
-		// = heap 영역으로 할당
 		
+		
+		// 서비스 객체 생성
+		StudentService service = new StudentService(); // heap 영역으로 할당
+		
+		try {
+			
+			// 학생 전체 조회 서비스 호출 후 결과 반환 받기
+			List<Student> stdList = service.selectAll();
+			
+			// 조회 결과 request에 세팅
+			req.setAttribute("stdList", stdList);
+			
+			// 요청 위임
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/stduent/selectAll.jsp");
+			
+			dispatcher.forward(req, resp);
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 	}
 }
