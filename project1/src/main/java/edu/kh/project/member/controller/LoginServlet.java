c5e3795870f3582d34300cc96faec00900c16694
rkdalsrb65 <rkdalsrb65@naver.com>
2022-10-18 12:40:27 +0900

    LoginServlet

--- project1/src/main/java/edu/kh/project/member/controller/LoginServlet.java
+++ project1/src/main/java/edu/kh/project/member/controller/LoginServlet.java
@@ -2,6 +2,7 @@ package edu.kh.project.member.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,8 +23,8 @@ public class LoginServlet extends HttpServlet{
 		try {
 			
 			// 파라미터 얻어오기
-			String inputEmail = getInitParameter("inputEmail");
-			String inputPw = getInitParameter("inputPw");
+			String inputEmail = req.getParameter("inputEmail");
+			String inputPw = req.getParameter("inputPw");
 			
 			Member member = new Member();
 			member.setMemberEmail(inputEmail);
@@ -31,6 +32,21 @@ public class LoginServlet extends HttpServlet{
 			
 			// 로그인 Service 호출 후 결과 반환 받기
 			Member loginMember = service.login(member);
+			
+			// forward를 하는 경우
+			// - 요청을 다른 Servlet/JSP로 위임
+			// -> 어떤 요청이 위임됐는지 알아야 되기 때문에
+			//		주소창에 요청 주소가 계속 남아있다.
+			// - 
+//			RequestDispatcher dispatcher =  req.getRequestDispatcher("/WEB-INF/views/common/main.jsp");
+//			
+//			req.setAttribute("loginMember", loginMember);
+//			
+//			dispatcher.forward(req, resp);
+			
+			// *** redirect(재요청) ***
+			// - servlet이 다른 주소를 요청함.
+			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
