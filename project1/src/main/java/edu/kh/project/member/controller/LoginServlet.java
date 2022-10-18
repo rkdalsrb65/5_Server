27d4e6f43bb3d06cfa518c1f4342d919a5e9c5c4
rkdalsrb65 <rkdalsrb65@naver.com>
2022-10-18 15:38:47 +0900

    LoginServlet

--- project1/src/main/java/edu/kh/project/member/controller/LoginServlet.java
+++ project1/src/main/java/edu/kh/project/member/controller/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import edu.kh.project.member.model.service.MemberService;
 import edu.kh.project.member.model.vo.Member;
@@ -49,6 +50,30 @@ public class LoginServlet extends HttpServlet{
 			// - 요청에 대한 응답화면을 직접 만드는 거이 아닌
 			// 다른 응답화면을 구현하는 Servlet을 요청하여
 			// 대신 화면을 만들게 하는 것.
+			
+			// request scope에 속성을 추가해도
+			// redirect를 하는 경우 request가 다시 만들어져 유지되지 않음 !
+//			req.setAttribute("loginMember", loginMember);
+			
+			// 해결 방법 : Session scope 이용
+			
+			// 1) HttpSession 객체 얻어오기
+			HttpSession session = req.getSession();
+			
+			if(loginMember != null) { // 로그인 성공 시
+			
+			// 2) Session scope에 속성 추가하기
+			session.setAttribute("loginMember", loginMember);
+			
+			// 3) 세션 만료시간 설정(초 단위로 지정)
+			// 		(클라이언트가 새로운 요청을 할 때 마다 초기화)
+			session.setMaxInactiveInterval(60 * 60); // 60초 * 60초 == 1시간(3600초)
+
+			} else { // 로그인 실패 시
+				
+				session.setAttribute("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
+				
+			}
 			
 			// 메인 페이지로 redirect
 			// -> 메인 페이지를 요청한 것이기 때문에
