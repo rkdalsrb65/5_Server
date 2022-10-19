e78650438a37985e1a260d68c82cd7077d571167
rkdalsrb65 <rkdalsrb65@naver.com>
2022-10-19 15:52:10 +0900

    SignUpServlet

---
+++ project1/src/main/java/edu/kh/project/member/controller/SignUpServlet.java
@@ -0,0 +1,29 @@
+package edu.kh.project.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/signUp")
+public class SignUpServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String path = "/WEB-INF/views/member/signUp.jsp";
+		
+		req.getRequestDispatcher(path).forward(req, resp);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
