bd5f25538c3d8db654ec2c67f88271b68261c3c5
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-10 16:43:13 +0900

    project.sql

--- project1/project.sql
+++ project1/project.sql
@@ -298,3 +298,19 @@ SELECT * FROM "MEMBER";
 
 DELETE "MEMBER"
 WHERE MEMBER_NO= '9';
+
+
+-- 탈퇴하지 않은 회원 중 이메일이 같은 사람의 수 조회
+-- 0 : 중복 X / 1 : 중복 O
+SELECT COUNT(*) FROM "MEMBER"
+WHERE MEMBER_EMAIL = 'user01@kh.or.kr'
+AND MEMBER_DEL_FL = 'N';
+
+SELECT * FROM "MEMBER";
+
+
+
+
+
+
+
